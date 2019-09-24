77fc625ef237ca8c71e0894d63c30969d715acb1
JQuinteroC <jlqc1772@gmail.com>
2019-09-24 16:25:12 -0500

    Se modifico el enrutamiento de las animaciones para el Duende

--- Juego/src/Logica/Duende.java
+++ Juego/src/Logica/Duende.java
@@ -21,22 +21,21 @@ public class Duende extends ConstruirPersonaje implements Cloneable {
     public void CrearAnimaciones() {
 
         caminar = new ImageIcon[24];
-        saltar = new ImageIcon[12];
+        saltar = new ImageIcon[18];
         morir = new ImageIcon[15];
         atacar = new ImageIcon[12];
         
         for (int i = 0; i < 24; i++) {
-            caminar[i] = new ImageIcon(new ImageIcon(getClass().getResource("/recursos/0_Goblin_Walking_" + i + ".png")).getImage());
+            caminar[i] = new ImageIcon(new ImageIcon(getClass().getResource("Recursos\\Goblin\\Walking\\0_Goblin_Walking_" + i + ".png")).getImage());
         }
-        for (int i = 0; i < 6; i++) {
-            saltar[i] = new ImageIcon(new ImageIcon(getClass().getResource("/recursos/0_Goblin_Jump Start_" + (i + 1) + ".png")).getImage());
-            saltar[11 - i] = new ImageIcon(new ImageIcon(getClass().getResource("/recursos/0_Goblin_Jump Start_" + (i + 1) + ".png")).getImage());
+        for (int i = 0; i < 18; i++) {
+            saltar[i] = new ImageIcon(new ImageIcon(getClass().getResource("Recursos\\Goblin\\Jump\\0_Goblin_Jump_" + i + ".png")).getImage());
         }
         for (int i = 0; i < 15; i++) {
-            morir[i] = new ImageIcon(new ImageIcon(getClass().getResource("/recursos/0_Goblin_Dying_" + i + ".png")).getImage());
+            morir[i] = new ImageIcon(new ImageIcon(getClass().getResource("Recursos\\Goblin\\Die\\0_Goblin_Dying_" + i + ".png")).getImage());
         }
         for (int i = 0; i < 12; i++) {
-            atacar[i] = new ImageIcon(new ImageIcon(getClass().getResource("/recursos/0_Goblin_Kicking_" + i + ".png")).getImage());
+            atacar[i] = new ImageIcon(new ImageIcon(getClass().getResource("Recursos\\Goblin\\Attack\\0_Goblin_Slashing_" + i + ".png")).getImage());
         }
         
         personaje.setAtacar(atacar);

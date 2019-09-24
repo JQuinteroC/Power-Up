f111ba026f93430b3a87319141816d3e4519d7fd
JQuinteroC <jlqc1772@gmail.com>
2019-09-24 13:53:18 -0500

    Corrección en Selector

--- Juego/src/GUI/FRM_Selector.java
+++ Juego/src/GUI/FRM_Selector.java
@@ -1,6 +1,12 @@
 package GUI;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 /**
@@ -8,27 +14,35 @@ import javax.swing.ImageIcon;
  * @author <a href="https://github.com/JQuinteroC">JQuinteroC</a>
  */
 public class FRM_Selector extends javax.swing.JFrame {
+
     private static FRM_Selector GUI;
-    
+
     /**
      * Creates new form FRM_Selector
      */
-    private FRM_Selector() {
+    private FRM_Selector() throws IOException {
         initComponents();
         super.setLocationRelativeTo(null);
+        Image s = ImageIO.read(new File("Recursos\\Wizard\\Idle\\1_IDLE_000.png"));
+        ImageIcon l = new ImageIcon(s.getScaledInstance(387, 350, Image.SCALE_SMOOTH));
+        lblMago.setIcon(l);
         
-        Image img= new ImageIcon("/GUI/0_Goblin_Idle_000.png").getImage();
-        ImageIcon img2=new ImageIcon(img.getScaledInstance(300, 300, Image.SCALE_SMOOTH));
-        lblOgro.setIcon(img2);
+        s = ImageIO.read(new File("Recursos\\Goblin\\Idle\\Goblin_Idle_000.png"));
+        l = new ImageIcon(s.getScaledInstance(387, 350, Image.SCALE_SMOOTH));
+        lblOgro.setIcon(l);
     }
-    
-    public static FRM_Selector getInstance() {
+
+    public static FRM_Selector getInstance() throws IOException {
         if (GUI == null) {
-            GUI = new FRM_Selector();
+            try {
+                GUI = new FRM_Selector();
+            } catch (IOException ex) {
+                Logger.getLogger(FRM_Selector.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         return GUI;
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -40,19 +54,19 @@ public class FRM_Selector extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lblMago.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/1_IDLE_000.png"))); // NOI18N
         lblMago.setFocusable(false);
         lblMago.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblMago.setMaximumSize(new java.awt.Dimension(387, 350));
+        lblMago.setMinimumSize(new java.awt.Dimension(387, 350));
+        lblMago.setPreferredSize(new java.awt.Dimension(387, 350));
         lblMago.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 lblMagoMouseClicked(evt);
             }
         });
 
-        lblOgro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/0_Goblin_Idle_000.png"))); // NOI18N
         lblOgro.setToolTipText("");
         lblOgro.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        lblOgro.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         lblOgro.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
         lblOgro.setMaximumSize(new java.awt.Dimension(387, 350));
         lblOgro.setMinimumSize(new java.awt.Dimension(387, 350));
@@ -67,33 +81,35 @@ public class FRM_Selector extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(jPanel1Layout.createSequentialGroup()
-                .add(24, 24, 24)
-                .add(lblMago)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 59, Short.MAX_VALUE)
-                .add(lblOgro, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .add(30, 30, 30))
-            .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .add(jLabel2)
                 .add(336, 336, 336))
+            .add(jPanel1Layout.createSequentialGroup()
+                .add(21, 21, 21)
+                .add(lblMago, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(28, 28, 28)
+                .add(lblOgro, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel1Layout.createSequentialGroup()
-                .add(28, 28, 28)
+            .add(jPanel1Layout.createSequentialGroup()
+                .add(14, 14, 14)
                 .add(jLabel2)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 51, Short.MAX_VALUE)
-                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(lblOgro, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(lblMago))
-                .add(47, 47, 47))
+                .add(18, 18, 18)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(lblMago, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(lblOgro, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(39, Short.MAX_VALUE))
         );
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
+                .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
